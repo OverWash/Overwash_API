c94babab40614ad374ebe33855f0200ec1f8684b
sojung lee <sojong1003@gmail.com>
2023-03-27 14:17:38 +0900

    FEAT : 가격리스트 페이징 처리 구현

--- src/main/java/com/meta/overwash/api/CommonRestController.java
+++ src/main/java/com/meta/overwash/api/CommonRestController.java
@@ -1,24 +1,18 @@
 package com.meta.overwash.api;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.meta.overwash.domain.CrewDTO;
-import com.meta.overwash.domain.LaundryDTO;
-import com.meta.overwash.domain.MemberDTO;
-import com.meta.overwash.domain.UserDTO;
+import com.meta.overwash.domain.*;
 import com.meta.overwash.service.CrewService;
 import com.meta.overwash.service.LaundryService;
 import com.meta.overwash.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CommonRestController {
@@ -32,6 +26,7 @@ public class CommonRestController {
     @Autowired
     LaundryService laundryService;
 
+    // 멤버 회원가입 등록
     @PostMapping("/register/member")
     public ResponseEntity<MemberDTO> registerMember(@RequestBody ObjectNode saveObj) throws Exception {
         ObjectMapper mapper = new ObjectMapper();   // JSON 을  Object 로 매핑해주는 Jackson ObjectMapper 이용
@@ -42,6 +37,7 @@ public class CommonRestController {
         return ResponseEntity.status(HttpStatus.CREATED).body(member);
     }
 
+    // 크루 회원가입 등록
     @PostMapping("/register/crew")
     public ResponseEntity<CrewDTO> registerCrew(@RequestBody ObjectNode saveObj) throws Exception {
         ObjectMapper mapper = new ObjectMapper();   // JSON 을  Object 로 매핑해주는 Jackson ObjectMapper 이용
@@ -52,10 +48,19 @@ public class CommonRestController {
         return ResponseEntity.status(HttpStatus.CREATED).body(crew);
     }
 
-    // 가격리스트
-    @GetMapping("/info/price")
-    public List<LaundryDTO> priceList() {
-        return laundryService.getList();
-    }
+//    // 가격리스트
+//    @GetMapping("/info/price")
+//    public List<LaundryDTO> priceList() {
+//        return laundryService.getList();
+//    }
 
+    // 가격리스트 페이징 처리
+    @GetMapping("/info/price")
+    public ResponseEntity<Map<String, Object>> priceListPaging(Criteria cri,
+                                                               @RequestParam(required = false, defaultValue = "1") int page,
+                                                               @RequestParam(required = false, defaultValue = "10") int amount) throws Exception {
+		cri.setPageNum(page);
+		cri.setAmount(amount);
+		return new ResponseEntity<Map<String, Object>>(laundryService.getListPaging(cri), HttpStatus.OK);
+	}
 }
--- src/main/java/com/meta/overwash/service/LaundryServiceImpl.java
+++ src/main/java/com/meta/overwash/service/LaundryServiceImpl.java
@@ -1,15 +1,17 @@
 package com.meta.overwash.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.meta.overwash.domain.Criteria;
+import com.meta.overwash.domain.PagenationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.meta.overwash.domain.LaundryDTO;
 import com.meta.overwash.domain.WashingCompleteDTO;
 import com.meta.overwash.mapper.LaundryMapper;
-
-import lombok.extern.log4j.Log4j;
 
 @Service
 public class LaundryServiceImpl implements LaundryService {
@@ -24,9 +26,22 @@ public class LaundryServiceImpl implements LaundryService {
 	}
 
 	@Override
+	public Map<String, Object> getListPaging(Criteria cri) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("laundryPaging", new PagenationDTO(cri, getLaundryCnt()));
+		map.put("laundries", laundryMapper.getListPaging(cri));
+		return map;
+	}
+
+	@Override
 	public WashingCompleteDTO updateWashComplete() {
 
 		return null;
+	}
+
+	// 페이징 처리용 메소드
+	private int getLaundryCnt() {
+		return laundryMapper.getLaundryCnt().intValue();
 	}
 
 }
