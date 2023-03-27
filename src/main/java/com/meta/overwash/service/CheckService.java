696d20d720dad3238b746a8c7e451eeea679c5b8
sojung lee <sojong1003@gmail.com>
2023-03-27 14:19:24 +0900

    FEAT : 검수내역 페이징 처리 구현

--- src/main/java/com/meta/overwash/service/CheckService.java
+++ src/main/java/com/meta/overwash/service/CheckService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.Map;
 
 import com.meta.overwash.domain.CheckDTO;
+import com.meta.overwash.domain.Criteria;
 
 public interface CheckService {
 
 	public List<CheckDTO> getCheckList(Long confirmId);
+
+	public Map<String, Object> getCheckListPaging(Criteria cri, Long confirmId);
 }
